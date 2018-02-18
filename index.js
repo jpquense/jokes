c95b1a4ba2939903fe49809151b665943b98d6c6
John P Quense <jpquense@gmail.com>
2018-02-18 11:05:06 -0500

    js file api added

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$('.js-btn').click(e => {
+	generateJoke();
+})
+
+function generateJoke() {
+	$.getJSON('https://icanhazdadjoke.com', renderJoke);
+}
+
+function renderJoke(response) {
+	const colors = ['f8a055','#ffdb5c', '#fa6e59'];               
+	const rand = Math.floor(Math.random()*colors.length);
+	
+	$('.js-quote').html(`
+		<span class="quotes">❝</span>
+		<p>${response.joke}</p>
+		<footer class="blockquote-footer">Someone funny from <cite title="Source Title">icanhazdadjoke</cite>
+		`);
+	$('.js-color').css('background-color', colors[rand]);
+}
+																								 
+         
+  
